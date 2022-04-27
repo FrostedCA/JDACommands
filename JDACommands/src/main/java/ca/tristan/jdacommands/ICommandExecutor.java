665e6425b1702d6b30305c217b666c1a58cc131b
Tristan <contact.tristan.landry@gmail.com>
2022-04-27 14:48:29 -0400

    0.1.0
    First open-source version of JDACommands.

---
+++ JDACommands/src/main/java/ca/tristan/jdacommands/ICommandExecutor.java
@@ -0,0 +1,20 @@
+package ca.tristan.jdacommands;
+
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.*;
+
+public interface ICommandExecutor {
+
+    void execute(Command event);
+
+    String getName();
+
+    String helpMessage();
+
+    boolean needOwner();
+
+    default Role canUse(Guild guild){
+        return guild.getPublicRole();
+    }
+
+}
